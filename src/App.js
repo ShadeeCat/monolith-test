1e22820826f65cec3e5267caba1023884b3507b1
Alexander Sulema <assulema@gmail.com>
2019-10-18 13:03:24 +0300

    started the product page with general header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+
+import GeneralHeader from "./components/generalHeader";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <GeneralHeader />
     </div>
   );
 }
---
+++ src/components/generalHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class GeneralHeader extends React.Component{
+    render() {
+        return <header>
+            <div>
+                <h1>SPECTRUM</h1>
+            </div>
+            <nav>
+                <a href="# ">HOME</a>
+                <a href="# ">CART</a>
+            </nav>
+        </header>
+    }
+}
